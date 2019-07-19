b93327365119d24b9f8f2f60a97e500bc5eb9ddd
ffulauh <hshua24@gmail.com>
2019-07-19 11:51:12 +0800

    修改数据库脚本

--- src/main/dbscript/dps_user.sql
+++ src/main/dbscript/dps_user.sql
@@ -1,13 +1,14 @@
-CREATE TABLE user
-(
-  id            INT AUTO_INCREMENT
-    PRIMARY KEY,
-  username      VARCHAR(24)            NOT NULL,
-  password      VARCHAR(24)            NOT NULL,
-  fullName      VARCHAR(96)            NOT NULL,
-  email         VARCHAR(48)            NOT NULL,
-  updateByEmail TINYINT(1) DEFAULT '0' NOT NULL
-);
+use dps;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(24) NOT NULL,
+  `password` varchar(24) NOT NULL,
+  `fullName` varchar(96) NOT NULL,
+  `email` varchar(48) NOT NULL,
+  `updateByEmail` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO dps.user (username, password, fullName, email, updateByEmail) VALUES ('hsh', '02042018', '韩少华', 'hshua24@gmail.com', 0);
 INSERT INTO dps.user (username, password, fullName, email, updateByEmail) VALUES ('steve', '22222', '乔布斯', 'steve@apple.com', 0);
